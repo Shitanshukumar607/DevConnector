4f2cecfebe77a0d15f81331d1a18350dd5f0cc44
Shitanshu Kumar <shitanshukumar639@gmail.com>
2025-05-31 18:58:00 +0530

    feat: set up server with MongoDB connection and user, post and comment models

---
+++ server/src/models/user.model.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const UserSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      lowercase: true,
+      minLength: 3,
+      maxLength: 20,
+    },
+    fullName: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
+    phoneNumber: {
+      type: String,
+      trim: true,
+    },
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+const User = mongoose.model("User", UserSchema);
+export default User;
