8f4fc26f1296b360c7588aaeb5254455ec512006
Shitanshu Kumar <shitanshukumar639@gmail.com>
2025-06-05 01:34:59 +0530

    fix: update baseUrl  for production environment

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,3 +1,7 @@
-const baseUrl: string = "http://localhost:3000/api/v1";
+let baseUrl: string = "http://localhost:3000/api/v1";
+
+if (import.meta.env.MODE === "production") {
+  baseUrl = "https://devconnector-j917.onrender.com/api/v1";
+}
 
 export { baseUrl };
