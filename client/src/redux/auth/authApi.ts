64765e9ae5412b794e33890ffeae8b521c78293a
Shitanshu Kumar <shitanshukumar639@gmail.com>
2025-06-05 01:14:05 +0530

    feat: add authApi and authSlice for user authentication

---
+++ client/src/redux/auth/authApi.ts
@@ -0,0 +1,42 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { baseUrl } from "../../constants";
+
+type RegisterData = {
+  fullName: string;
+  email: string;
+  password: string;
+};
+
+type LoginData = {
+  email: string;
+  password: string;
+};
+
+export const authApi = createApi({
+  reducerPath: "authApi",
+
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${baseUrl}/users/`,
+    credentials: "include",
+  }),
+
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (data: RegisterData) => ({
+        url: "register",
+        method: "POST",
+        body: data,
+      }),
+    }),
+
+    login: builder.mutation({
+      query: (data: LoginData) => ({
+        url: "login",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useRegisterMutation, useLoginMutation } = authApi;
